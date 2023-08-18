1acf8fccca27c3cd47b2a6d15789b6e7c04915b2
Alejandro Gutiérrez <alejandrogutierrezchaves@gmail.com>
2023-08-18 11:31:25 -0600

    añadir juego

---
+++ cargame/src/track/Obstaculo.java
@@ -0,0 +1,15 @@
+package track;
+
+public class Obstaculo {
+	private String Tipo;
+	private int duracion;
+	private int damage;
+	
+	public Obstaculo(String pTipo) {
+	
+	this.Tipo = pTipo;
+	duracion = 5;
+	damage = 7;
+
+	}
+}
